2223acfb1b9d79425b0fe6a47a21f56af564a7e1
Jaewook Ahn <ajw4586@gmail.com>
2021-07-27 17:14:34 +0900

    Add test page to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {Layout} from './components';
+import * as pages from './pages';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Layout>
+        <Switch>
+          <Route path="/" exact component={pages.Landing} />
+          <Route path="/test" component={pages.Test} />
+        </Switch>
       </Layout>
     </BrowserRouter>
   );
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import styled from 'styled-components';
 import {Button} from '../components';
 
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 16px 24px;
+`;
+
 export const Test = () => {
+  const [count, setCount] = useState(0);
+  const handleClick = useCallback(() => {
+    setCount(count + 1);
+  }, [count, setCount]);
   return (
-    <div>
-      <h1>BUtton</h1>
-      <Button>Button 1</Button>
-    </div>
+    <Wrapper>
+      <h2>Button</h2>
+      <Button mr="8px">Button 1</Button>
+      <Button onClick={handleClick}>{count} time clicked</Button>
+    </Wrapper>
   );
 };
