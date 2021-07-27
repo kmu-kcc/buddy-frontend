ba24aaed3b7925e51a1f8bd15c38b345ff6d62c0
Jaewook Ahn <ajw4586@gmail.com>
2021-07-28 03:52:16 +0900

    Add lint rules for react and jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,18 @@ module.exports = {
   rules: {
     'require-jsdoc': 'off',
     'linebreak-style': 'off',
+    'jsx-quotes': ['warn', 'prefer-single'],
+    'react/jsx-space-before-closing': ['warn', 'always'],
+    'react/function-component-definition': ['error', {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
+    'react/jsx-closing-tag-location': ['warn'],
+    'react/jsx-curly-spacing': ['warn', {
+      when: 'never',
+      allowMultiline: false,
+    }],
+    'react/jsx-equals-spacing': ['warn', 'never'],
+    'react/jsx-props-no-multi-spaces': ['warn'],
   },
 };
