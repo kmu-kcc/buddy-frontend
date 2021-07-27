7e9ab57dbf4d1cedebaed142d1f107f28b494987
Jaewook Ahn <ajw4586@gmail.com>
2021-07-27 15:15:14 +0900

    Add /test page for component testing
    - update header title
    - update global layout style

---
+++ src/pages/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from '../components';
+
+export const Test = () => {
+  return (
+    <div>
+      <h1>BUtton</h1>
+      <Button>Button 1</Button>
+    </div>
+  );
+};
