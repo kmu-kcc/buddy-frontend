a0c33756009e2e4a64daa23c6483464210053976
Jaewook Ahn <ajw4586@gmail.com>
2021-07-28 19:36:08 +0900

    Fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
           <Route path='/test' component={pages.Test} />
         </Switch>
       </Layout>
-      <Navbar/>
+      <Navbar />
     </BrowserRouter>
   );
 };
