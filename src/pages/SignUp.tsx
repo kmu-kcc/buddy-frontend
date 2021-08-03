7adb25fca94952d9e4ab8c94a5d45a6ff8604c6f
GoRyne <korsogogii@kookmin.ac.kr>
2021-08-03 22:45:48 +0900

    Fix lint errors

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,66 +4,66 @@ export const SignUp: React.FC = () => {
   return (
     <div>
       <div>학번을 입력해주세요</div>
-      <input placeholder = "학번"></input>
+      <input placeholder='학번'></input>
       <br></br>
       <br></br>
       <div>비밀번호를 입력해주세요</div>
-      <input placeholder = "비밀번호"></input>
+      <input placeholder='비밀번호'></input>
       <br></br>
       <br></br>
       <div>비밀번호를 다시한번 입력해주세요</div>
-      <input placeholder = "비밀번호 재입력"></input>
+      <input placeholder='비밀번호 재입력'></input>
       <button>비밀번호 확인</button>
       <br></br>
       <br></br>
       <div>대학를 선택해주세요</div>
-      <select name="학부" id="major-check">
-        <option value="">----------------------------------</option>
-        <option value="글로벌인문지역대학">글로벌인문지역대학</option>
-        <option value="사회과학대학">사회과학대학</option>
-        <option value="법과대학">법과대학</option>
-        <option value="경상대학">경상대학</option>
-        <option value="경영대학">경영대학</option>
-        <option value="창의공과대학">창의공과대학</option>
-        <option value="과학기술대학">과학기술대학</option>
-        <option value="예술대학">예술대학</option>
-        <option value="체육대학">체육대학</option>
-        <option value="조형대학">조형대학</option>
-        <option value="소프트웨어융합대학">소프트웨어융합대학</option>
-        <option value="건축대학">건축대학</option>
-        <option value="자동차융합대학">자동차융합대학</option>
-        <option value="">----------------------------------</option>
+      <select name='학부' id='major-check'>
+        <option value=''>----------------------------------</option>
+        <option value='글로벌인문지역대학'>글로벌인문지역대학</option>
+        <option value='사회과학대학'>사회과학대학</option>
+        <option value='법과대학'>법과대학</option>
+        <option value='경상대학'>경상대학</option>
+        <option value='경영대학'>경영대학</option>
+        <option value='창의공과대학'>창의공과대학</option>
+        <option value='과학기술대학'>과학기술대학</option>
+        <option value='예술대학'>예술대학</option>
+        <option value='체육대학'>체육대학</option>
+        <option value='조형대학'>조형대학</option>
+        <option value='소프트웨어융합대학'>소프트웨어융합대학</option>
+        <option value='건축대학'>건축대학</option>
+        <option value='자동차융합대학'>자동차융합대학</option>
+        <option value=''>----------------------------------</option>
       </select>
       <br></br>
       <br></br>
       <div>소속학과를 입력해주세요</div>
-      <input placeholder = "소속학과"></input>
+      <input placeholder='소속학과'></input>
       <br></br>
       <br></br>
       <div>학년을 선택해주세요</div>
-      <select name = "SchoolYear" id = "year check">
-        <option value="">----</option>
-        <option value="Freshman">1</option>
-        <option value="Sophemore">2</option>
-        <option value="junior">3</option>
-        <option value="senior">4</option>
-        <option value="other">그 외</option>
-        <option value="">----</option>
+      <select name='SchoolYear' id='year check'>
+        <option value=''>----</option>
+        <option value='Freshman'>1</option>
+        <option value='Sophemore'>2</option>
+        <option value='junior'>3</option>
+        <option value='senior'>4</option>
+        <option value='other'>그 외</option>
+        <option value=''>----</option>
       </select>
       <br></br>
       <br></br>
       <div>핸드폰 번호를 입력해주세요</div>
-      <input placeholder = "핸드폰 번호 입력"></input>
+      <input placeholder='핸드폰 번호 입력'></input>
       <br></br>
       <br></br>
       <div>이메일을 입력해주세요</div>
-      <input placeholder = "이메일 입력"></input>
+      <input placeholder='이메일 입력'></input>
       <br></br>
       <br></br>
       <div>휴학 여부를 선택해주세요</div>
-      <select name = "휴학" id = "M-Term">
-        <option value = "true">휴학</option>
-        <option value = "false">재학</option>
+      <select name='휴학' id='M-Term'>
+        <option value='true'>휴학</option>
+        <option value='false'>재학</option>
       </select>
     </div>
   );
