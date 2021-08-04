4c5b34ea8270a173e2c2af9266309686fd87f9db
Jaewook Ahn <ajw4586@gmail.com>
2021-08-04 16:24:33 +0900

    Fix scale props in Icon to work

--- src/components/icons/Arrow.tsx
+++ src/components/icons/Arrow.tsx
@@ -13,11 +13,10 @@ export const Arrow = (props: Props) => {
   const {scale = 1, color, rotate = 0, ...styles} = props;
   const width = useMemo(() => 16 * scale, [scale]);
   const height = useMemo(() => 11 * scale, [scale]);
-  const viewBox = useMemo(() => `0 0 ${width} ${height}`, [width, height]);
   const rotateStyle = useMemo(() => ({transform: `rotate(${rotate}deg)`}), [rotate]);
 
   return (
-    <Icon width={width} height={height} viewBox={viewBox} fill='none' style={rotateStyle} {...styles}>
+    <Icon width={width} height={height} viewBox='0 0 16 11' style={rotateStyle} {...styles}>
       <path d='M1 1L8 10L15 1' stroke={color ?? '#6D48E5'} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
     </Icon>
   );
