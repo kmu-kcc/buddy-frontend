c04f49b72ec67ee6cc397366fe7035c6efc088b5
Jaewook Ahn <ajw4586@gmail.com>
2021-08-04 17:31:22 +0900

    Refactor Check component
    - add check icon
    - improve component css
    
    Known Issues
    - check svg icon not center aligned

--- src/components/Check.tsx
+++ src/components/Check.tsx
@@ -1,41 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import {space, SpaceProps} from 'styled-system';
+import {Check as CheckIcon} from './icons/Check';
 
-const Text = styled.span`
-  height: 11px;
-  color: #454440;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 9px;
-  line-height: 11px;
-  margin-left: 6px;
-`;
-
-const Checkbox = styled.div<{isChecked: boolean;}>`
-  height: 10px;
-  width: 10px;
-  border-radius: 1px;
-  border: 1px solid ${({isChecked}) => isChecked ? '#6D48E5' : '#CBC8BE'};
-  display: inline-block;
-  background-color: ${({isChecked}) => isChecked ? '#6D48E5' : '#fff'};
-`;
-
-const Wrapper = styled.div<SpaceProps & {isChecked: boolean;}>`
+const Wrapper = styled.div<SpaceProps & {checked: boolean;}>`
   ${space}
-  user-select: none;
   width: fit-content;
   display: inline-flex;
+  user-select: none;
   align-items: flex-start;
   cursor: pointer;
 
-
   :hover {
     > div {
-      border-color: '#6D48E5';
-      background-color: ${({isChecked}) => isChecked ? '#6D48E5' : '#EFEBFC'};
+      border-color: #6D48E5;
+      background-color: ${({checked}) => checked ? '#6D48E5' : '#EFEBFC'};
     }
   }
+
+  > div {
+    border: 1px solid ${({checked}) => checked ? '#6D48E5' : '#CBC8BE'};
+    background-color: ${({checked}) => checked ? '#6D48E5' : '#fff'};
+
+    > svg {
+      visibility: ${({checked}) => checked ? 'visible' : 'hidden'};
+      margin: auto;
+    }
+  }
+`;
+
+const Box = styled.div`
+  box-sizing: border-box;
+  height: 10px;
+  width: 10px;
+  display: inherit;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1px;
+  transition: all 0.15s ease-out;
+`;
+
+const Text = styled.span`
+  color: #454440;
+  font-size: 9px;
+  line-height: 11px;
+  margin-left: 6px;
 `;
 
 interface Props extends SpaceProps {
@@ -48,8 +57,10 @@ interface Props extends SpaceProps {
 export const Check = (props: Props) => {
   const {label, onCheck, checked, ...styles} = props;
   return (
-    <Wrapper isChecked={checked} onClick={onCheck} {...styles}>
-      <Checkbox isChecked={checked} />
+    <Wrapper checked={checked} onClick={onCheck} {...styles}>
+      <Box>
+        <CheckIcon color='#fff' />
+      </Box>
       <Text>{label}</Text>
     </Wrapper>
   );
