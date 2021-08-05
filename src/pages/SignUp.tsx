ceef90e5c88280f69dc861025ee0a9e84386c23d
Jaewook Ahn <ajw4586@gmail.com>
2021-08-06 04:01:16 +0900

    Refactor signup page state and handler
    - improve variable name
    - optimize input handlers

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -10,40 +10,28 @@ const Text = styled.p`
 `;
 
 export const SignUp: React.FC = () => {
-  const [inputIDValue, setInputIDValue] = useState('');
-  const handleIDChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputIDValue(event.target.value);
-  }, [setInputIDValue]);
-  const [inputPasswordValue, setInputPasswordValue] = useState('');
-  const handlePasswordValue = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputPasswordValue(event.target.value);
-  }, [setInputPasswordValue]);
-  const [inputMajorValue, setInputMajorValue] = useState('');
-  const handleMajorValue = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputMajorValue(event.target.value);
-  }, [setInputMajorValue]);
-  const [inputPhoneNumber, setPhoneNumber] = useState('');
-  const handlePhoneNumber = useCallback((event:React.ChangeEvent<HTMLInputElement>) => {
-    setPhoneNumber(event.target.value);
-  }, [setPhoneNumber]);
-  const [inputMailAddress, setMailAddress] = useState('');
-  const handleMailAddress = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setMailAddress(event.target.value);
-  }, [setMailAddress]);
-  const [inputStudentNumber, setStudentNumber] = useState('');
-  const handleStudentNumber = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setStudentNumber(event.target.value);
-  }, [setStudentNumber]);
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [major, setMajor] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [studentNumber, setStudentNumber] = useState('');
+  const handleInputChange = useCallback((setState: React.Dispatch<React.SetStateAction<string>>) => {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      setState(event.target.value);
+    };
+  }, []);
+
   return (
     <Box isFlex width='100%' justifyContent='center'>
       <Box isFlex flexDirection='column' justifyContent='center' alignItems='center'>
         <Box isFlex>
           <Box>
-            <Input value={inputIDValue} onChange={handleIDChange} placeholder='이름' />
+            <Input value={id} onChange={handleInputChange(setId)} placeholder='이름' />
             <Text>한글만,최대10자</Text>
           </Box>
           <Box ml='174px'>
-            <Input value={inputPasswordValue} onChange={handlePasswordValue} placeholder='비밀번호' />
+            <Input value={password} onChange={handleInputChange(setPassword)} placeholder='비밀번호' />
             <Text>영어 대문자,숫자,특수문자 각 1개 이상 혼용, 전체 8글자 이상</Text>
           </Box>
         </Box>
@@ -66,13 +54,13 @@ export const SignUp: React.FC = () => {
             </Select>
           </Box>
           <Box ml='174px'>
-            <Input value={inputMajorValue} onChange={handleMajorValue} placeholder='소속학과' />
+            <Input value={major} onChange={handleInputChange(setMajor)} placeholder='소속학과' />
             <Text>한글만, 최대 15자</Text>
           </Box>
         </Box>
         <Box isFlex>
           <Box>
-            <Input value={inputStudentNumber} onChange={handleStudentNumber} placeholder='학번' />
+            <Input value={studentNumber} onChange={handleInputChange(setStudentNumber)} placeholder='학번' />
             <Text>영어와 숫자만 8~9자</Text>
           </Box>
           <Box ml='174px'>
@@ -96,12 +84,12 @@ export const SignUp: React.FC = () => {
             </Select>
           </Box>
           <Box ml='174px'>
-            <Input value={inputPhoneNumber} onChange={handlePhoneNumber} placeholder='핸드폰 번호' />
+            <Input value={phoneNumber} onChange={handleInputChange(setPhoneNumber)} placeholder='휴대폰 번호' />
             <Text>예) 010-1234-5678</Text>
           </Box>
         </Box>
         <Box width='100%'>
-          <Input mr='auto' value={inputMailAddress} onChange={handleMailAddress} placeholder='이메일' />
+          <Input mr='auto' value={email} onChange={handleInputChange(setEmail)} placeholder='이메일' />
         </Box>
         <Button mt='121px'>회원가입하기</Button>
       </Box>
