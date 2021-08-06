490bb03a9fbb08cad752c0ced929a7c1a004f055
Jaewook Ahn <ajw4586@gmail.com>
2021-08-06 11:14:10 +0900

    Fix Check icon component style props to work

--- src/components/icons/Check.tsx
+++ src/components/icons/Check.tsx
@@ -9,12 +9,12 @@ interface Props extends SpaceProps, CursorProps {
 }
 
 export const Check = (props: Props) => {
-  const {color, scale = 1} = props;
+  const {color, scale = 1, ...styles} = props;
   const width = useMemo(() => 6 * scale, [scale]);
   const height = useMemo(() => 5 * scale, [scale]);
 
   return (
-    <Icon width={width} height={height} viewBox='0 0 6 5'>
+    <Icon width={width} height={height} viewBox='0 0 6 5' {...styles}>
       <path d='M0.5 2L2.5 4L5.5 0.5' stroke={color ?? '#fff'} strokeLinecap='round' strokeLinejoin='round' />
     </Icon>
   );
