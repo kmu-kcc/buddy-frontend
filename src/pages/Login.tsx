5132ac1dded5e61d48e07843a18a95782972930b
Jia <jia5232@naver.com>
2021-08-09 17:44:36 +0900

    fix Loginpage detail
    Change page details by referring to code review

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,10 @@
 import React, {useCallback, useState} from 'react';
-import {Input, Button, Box, Check} from '../components';
-import styled from 'styled-components';
 import {Link} from 'react-router-dom';
+import {Input, Button, Box, Check} from '../components';
 import {Buddy} from '../components/icons';
 
-const Wrapper = styled.div`
-  width: 100%;
-  padding: 16px 24px;
-`;
 
-export const Login: React.FC = () => {
+export const Login = () => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
   const handleInputChange = useCallback((setState: React.Dispatch<React.SetStateAction<string>>) => {
@@ -17,39 +12,42 @@ export const Login: React.FC = () => {
       setState(event.target.value);
     };
   }, []);
-  const [check, setCheck] = useState(false);
+  const [idSaved, setIdSaved] = useState(false);
 
   const handleCheck = useCallback(() => {
-    setCheck(!check);
-  }, [check, setCheck]);
+    setIdSaved(!idSaved);
+  }, [idSaved, setIdSaved]);
 
   return (
-    <Wrapper>
-      <Box isFlex width='100%' justifyContent='center'>
-        <Box isFlex flexDirection='column' justifyContent='center' alignItems='center'>
-          <Box isBlock>
-            <Buddy mt='65px' mb='65px' width={70} height={106} color='#6D48E5' />
-          </Box>
-          <Box isBlock margin='10px'>
-            <Input value={id} onChange={handleInputChange(setId)} placeholder='아이디' />
-          </Box>
-          <Box isBlock margin='10px'>
-            <Input value={password} onChange={handleInputChange(setPassword)} placeholder='비밀번호' />
-          </Box>
-          <Button mt='65px' mb='40px'>로그인</Button>
-          <Box isInlineFlex style={{height: '30px'}}>
+    <Box isFlex width='100%' style={{padding: '16px 24px'}} justifyContent='center'>
+      <Box isFlex width='390px' flexDirection='column' alignItems='center'>
+        <Box isBlock mt='65px' mb='65px' >
+          <Buddy width={70} height={106} color='#6D48E5' />
+        </Box>
+        <Box isBlock margin='10px'>
+          <Input value={id} onChange={handleInputChange(setId)} placeholder='아이디' />
+        </Box>
+        <Box isBlock margin='10px'>
+          <Input value={password} onChange={handleInputChange(setPassword)} placeholder='비밀번호' />
+        </Box>
+        <Box mt='65px' mb='40px'>
+          <Button>로그인</Button>
+        </Box>
+        <Box isFlex width='100%'>
+          <Box mr='auto'>
             <Link to='/test' style={{textDecoration: 'none'}}>회원가입하기</Link>
-            <span style={{padding: '60px'}}></span>
+          </Box>
+          <Box>
             <Link to='/test' style={{textDecoration: 'none'}}>아이디 찾기</Link>
-            <span style={{padding: '4px'}}></span>
+          </Box>
+          <Box ml='16px'>
             <Link to='/test' style={{textDecoration: 'none'}}>비밀번호 찾기</Link>
           </Box>
-          <Box isBlock>
-            <span style={{padding: '160px'}}></span>
-            <Check boxShape='rectangle' mr='10px' onCheck={handleCheck} label='아이디 저장' checked={check} />
-          </Box>
+        </Box>
+        <Box isFlex width='100%' mt='10px'>
+          <Check ml='auto' boxShape='rectangle' mr='10px' onCheck={handleCheck} label='아이디 저장' checked={idSaved} />
         </Box>
       </Box>
-    </Wrapper>
+    </Box>
   );
 };
