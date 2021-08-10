af47609afb14a05e4dd8498749e87288b92e47fb
Jaewook Ahn <ajw4586@gmail.com>
2021-08-11 04:25:14 +0900

    Adjust login page layout components
    - fix icon props size
    - remove redundant elements
    - add components for Link

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,15 @@
 import React, {useCallback, useState} from 'react';
+import styled from 'styled-components';
+import {space, SpaceProps} from 'styled-system';
 import {Link} from 'react-router-dom';
 import {Input, Button, Box, Check} from '../components';
 import {Buddy} from '../components/icons';
 
+const LinkText = styled(Link)<SpaceProps>`
+  color:#363634;
+  text-decoration: none;
+  ${space}
+`;
 
 export const Login = () => {
   const [id, setId] = useState('');
@@ -19,33 +26,19 @@ export const Login = () => {
   }, [idSaved, setIdSaved]);
 
   return (
-    <Box isFlex width='100%' style={{padding: '16px 24px'}} justifyContent='center'>
-      <Box isFlex width='390px' flexDirection='column' alignItems='center'>
-        <Box isBlock mt='65px' mb='65px' >
-          <Buddy width={70} height={106} color='#6D48E5' />
+    <Box isFlex width='100%' justifyContent='center'>
+      <Box isFlex width='338px' flexDirection='column' alignItems='center' pt='120px'>
+        <Box isBlock mb='64px' >
+          <Buddy width='70px' height='106px' color='#6D48E5' />
         </Box>
-        <Box isBlock margin='10px'>
-          <Input value={id} onChange={handleInputChange(setId)} placeholder='아이디' />
-        </Box>
-        <Box isBlock margin='10px'>
-          <Input value={password} onChange={handleInputChange(setPassword)} placeholder='비밀번호' />
-        </Box>
-        <Box mt='65px' mb='40px'>
-          <Button>로그인</Button>
-        </Box>
+        <Input width='100%' value={id} onChange={handleInputChange(setId)} placeholder='아이디' />
+        <Input mt='20px' width='100%' value={password} onChange={handleInputChange(setPassword)} placeholder='비밀번호' />
+        <Check mt='16px' mr='auto' boxShape='circle' size='20px' fontSize='16px' lineHeight='19px' onCheck={handleCheck} label='아이디 저장' checked={idSaved} />
+        <Button mt='45px' mb='40px' width='100%'>로그인</Button>
         <Box isFlex width='100%'>
-          <Box mr='auto'>
-            <Link to='/test' style={{textDecoration: 'none'}}>회원가입하기</Link>
-          </Box>
-          <Box>
-            <Link to='/test' style={{textDecoration: 'none'}}>아이디 찾기</Link>
-          </Box>
-          <Box ml='16px'>
-            <Link to='/test' style={{textDecoration: 'none'}}>비밀번호 찾기</Link>
-          </Box>
-        </Box>
-        <Box isFlex width='100%' mt='10px'>
-          <Check ml='auto' boxShape='rectangle' mr='10px' onCheck={handleCheck} label='아이디 저장' checked={idSaved} />
+          <LinkText to='/signup'>회원가입</LinkText>
+          <LinkText to='/test' ml='auto'>아이디 찾기</LinkText>
+          <LinkText to='/test' ml='16px'>비밀번호 찾기</LinkText>
         </Box>
       </Box>
     </Box>
