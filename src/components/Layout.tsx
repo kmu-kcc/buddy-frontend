f94b856c2f36be3f2e996f39d6123f1e3d2b60f6
Jaewook Ahn <ajw4586@gmail.com>
2021-08-11 19:13:50 +0900

    Apply Navigation component to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Box} from './Box';
+import {Navigation} from './Navigation';
 
-const Wrapper = styled.div`
+const Wrapper = styled(Box)`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
   min-height: 100vh;
   background-color: #fff;
+  position: relative;
 `;
 
-const Content = styled.div`
-    display: flex;
+const Content = styled(Box)`
+  flex: 1;
 `;
 
 interface LayoutProps {
@@ -18,6 +24,7 @@ export const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
   const {children} = props;
   return (
     <Wrapper>
+      <Navigation />
       <Content>
         {children}
       </Content>
