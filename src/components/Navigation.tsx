b5551cac80fd5ed44646a7b53f87781e40f6f51f
Jaewook Ahn <ajw4586@gmail.com>
2021-08-11 19:55:33 +0900

    Add logo into Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,8 @@
-import React, {useMemo} from 'react';
+import React, {useCallback, useMemo} from 'react';
 import styled from 'styled-components';
 import {useHistory} from 'react-router-dom';
 import {Box} from './Box';
+import {Buddy} from './icons/Buddy';
 
 const Wrapper = styled(Box)`
   width: 240px;
@@ -11,7 +12,37 @@ const Wrapper = styled(Box)`
   left: 0;
   background-color: #6D48E5;
   border-radius: 0 20px 20px 0;
+  padding: 32px 0;
 `;
+
+const IconWrapper = styled(Box)`
+  color: #fff;
+  cursor: pointer;
+  border-radius: 6px;
+  transition: all 0.15s linear;
+
+  :hover {
+    background-color: rgba(0, 0, 0, 0.15);
+  }
+
+  > p {
+    margin: 0;
+    font-weight: 700;
+  }
+`;
+
+const Logo = () => {
+  const history = useHistory();
+  const handleClick = useCallback(() => {
+    history.push('/');
+  }, [history]);
+  return (
+    <IconWrapper isFlex alignItems='center' px='32px' py='20px' fontSize='28px' onClick={handleClick}>
+      <Buddy mr='16px' width='36px' height='50px' color='#fff' />
+      <p>Buddy</p>
+    </IconWrapper>
+  );
+};
 
 export const Navigation = () => {
   const history = useHistory();
@@ -26,6 +57,7 @@ export const Navigation = () => {
 
   return (
     <Wrapper display={hidden ? 'none' : 'block'}>
+      <Logo />
     </Wrapper>
   );
 };
