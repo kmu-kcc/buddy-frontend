409e3028ef0cbde30d2e09450ede49f967a2d1c4
Jia <jia5232@naver.com>
2021-08-13 16:41:34 +0900

    Modify index and app

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,5 @@ export {Test} from './Test';
 export {SignUp} from './SignUp';
 export {Login} from './Login';
 export {MemberApplication} from './MemberApplication';
+export {ModifyMypage} from './ModifyMypage';
+export {Mypage} from './Mypage';
