eeae09229b208e65eb5d41c90eb06a7c1c9e4308
Jaewook Ahn <ajw4586@gmail.com>
2021-08-14 05:07:08 +0900

    Improve ModifyMypage layout
    - refactor component structure
    - apply auto alignable flex css

--- src/pages/ModifyMypage.tsx
+++ src/pages/ModifyMypage.tsx
@@ -1,26 +1,15 @@
 import React, {useCallback, useState} from 'react';
-import {Input, Select, Button, Box} from '../components';
+import {Input, Select, Button, Box, Text} from '../components';
 import styled from 'styled-components';
 
-const HeaderText = styled.p`
-  color: #454440;
-  font-size: 40px;
-  font-weight: bold;
-  line-height: 50.08px;
-`;
-
-const MiddleText = styled.p`
-  color: #454440;
-  font-size: 24px;
-  font-weight: bold;
-  line-height: 30px;
-`;
-
-const Text = styled.p`
-  color: #454440;
+const FloatButton = styled(Button)`
+  width: 220px;
+  height: 72px;
+  position: fixed;
+  bottom: 35px;
+  right: 50px;
   font-size: 20px;
-  font-weight: bold;
-  line-height: 25px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
 `;
 
 export const ModifyMypage: React.FC = () => {
@@ -37,62 +26,37 @@ export const ModifyMypage: React.FC = () => {
   }, []);
 
   return (
-    <Box isBlock width='100%' justifyContent='center'>
-      <Box isFlex ml='60px' mt='60px'>
-        <HeaderText>마이페이지</HeaderText>
-      </Box>
-      <Box isFlex ml='60px' mt='20px'>
-        <MiddleText>수정하기</MiddleText>
-      </Box>
-      {/* 이름 */}
-      <Box isBlock mt='47px'>
-        <Box isInlineBlock ml='64px' mr='98px'>
-          <Text>이름</Text>
-        </Box>
-        <Box isInlineBlock>
+    <Box width='100%' pl='60px' pt='48px' pb='48px' position='relative'>
+      <Text color='#454440' fontSize='40px' lineHeight='50px' fontWeight={700}>마이페이지</Text>
+      <Text mt='20px' color='#454440' fontSize='24px' lineHeight='30px' fontWeight={700}>수정하기</Text>
+      <Box width='100%' pl='4px' mt='28px'>
+        {/* 이름 */}
+        <Box isFlex width='525px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>이름</Text>
           <Input value={name} onChange={handleInputChange(setName)} placeholder='홍길동'
             width='390px' height='63px' />
         </Box>
-      </Box>
-      {/* 비밀번호 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='60px' mt='25px'>
-          <Text>비밀번호</Text>
-        </Box>
-        <Box isInlineBlock>
-          <Input value={password} onChange={handleInputChange(setPassword)} placeholder='변경하기'
+        {/* 비밀번호 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>비밀번호</Text>
+          <Input value={password} type='password' onChange={handleInputChange(setPassword)} placeholder='변경하기'
             width='390px' height='63px' />
         </Box>
-      </Box>
-      {/* 전화번호 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='60px' mt='25px'>
-          <Text>전화번호</Text>
-        </Box>
-        <Box isInlineBlock>
-          <Input value={phoneNumber} onChange={handleInputChange(setPhoneNumber)}
+        {/* 전화번호 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>전화번호</Text>
+          <Input value={phoneNumber} type='tel' onChange={handleInputChange(setPhoneNumber)}
             placeholder='01012345678' width='390px' height='63px' />
         </Box>
-      </Box>
-      {/* 이메일 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='80px' mt='25px'>
-          <Text>이메일</Text>
-        </Box>
-        <Box isInlineBlock>
-          <Input value={email} onChange={handleInputChange(setEmail)} placeholder='abc@gmail.com'
+        {/* 이메일 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>이메일</Text>
+          <Input value={email} type='email' onChange={handleInputChange(setEmail)} placeholder='abc@gmail.com'
             width='390px' height='63px' />
         </Box>
-        <Box isInlineBlock ml='40px'>
-          <Button width='128px' height='46px' background='#EFEBFC' color='#6D48E5' px='0'>인증하기</Button>
-        </Box>
-      </Box>
-      {/* 대학 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='98px' mt='25px'>
-          <Text>대학</Text>
-        </Box>
-        <Box isInlineBlock>
+        {/* 대학 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>대학</Text>
           <Select placeholder='소속대학' width='390px' height='63px'>
             <option value='글로벌인문지역대학'>글로벌인문지역대학</option>
             <option value='사회과학대학'>사회과학대학</option>
@@ -109,33 +73,21 @@ export const ModifyMypage: React.FC = () => {
             <option value='자동차융합대학'>자동차융합대학</option>
           </Select>
         </Box>
-      </Box>
-      {/* 학과 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='98px' mt='25px'>
-          <Text>학과</Text>
-        </Box>
-        <Box isInlineBlock>
+        {/* 학과 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>학과</Text>
           <Input value={major} onChange={handleInputChange(setMajor)} placeholder='소속학과'
             width='390px' height='63px' />
         </Box>
-      </Box>
-      {/* 학번 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='98px' mt='25px'>
-          <Text>학번</Text>
-        </Box>
-        <Box isInlineBlock>
+        {/* 학번 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>학번</Text>
           <Input value={studentNumber} onChange={handleInputChange(setStudentNumber)}
             placeholder='학번 (ID)' width='390px' height='63px' />
         </Box>
-      </Box>
-      {/* 학년 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='98px' mt='25px'>
-          <Text>학년</Text>
-        </Box>
-        <Box isInlineBlock>
+        {/* 학년 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>학년</Text>
           <Select placeholder='학년' width='390px' height='63px'>
             <option>1</option>
             <option>2</option>
@@ -145,13 +97,9 @@ export const ModifyMypage: React.FC = () => {
             <option>6</option>
           </Select>
         </Box>
-      </Box>
-      {/* 재학여부 */}
-      <Box isBlock>
-        <Box isInlineBlock ml='64px' mr='60px' mt='25px'>
-          <Text>재학여부</Text>
-        </Box>
-        <Box isInlineBlock>
+        {/* 재학여부 */}
+        <Box isFlex width='525px' mt='25px' alignItems='center'>
+          <Text flex={1} color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>재학여부</Text>
           <Select placeholder='재학여부' width='390px' height='63px'>
             <option>재학</option>
             <option>휴학</option>
@@ -159,16 +107,12 @@ export const ModifyMypage: React.FC = () => {
             <option>기타</option>
           </Select>
         </Box>
-      </Box>
-      <Box isBlock ml='64px' mt='30px' mb='70px'>
-        <Button mt='65px' width='160px' height='50px' background='#FFEEEA'
-          color='#FF6845' border='1px solid #FF6845' px='0'>
+        <Button mt='47px' px='0' width='160px' height='50px' background='#FFEEEA' color='#FF6845'
+          border='1px solid #FF6845'>
             퇴부신청
         </Button>
       </Box>
-      <Box isBlock ml='1360px' mb='70px'>
-        <Button width='220px' height='72px'>저장하기</Button>
-      </Box>
+      <FloatButton width='220px' height='72px'>저장하기</FloatButton>
     </Box>
   );
 };
