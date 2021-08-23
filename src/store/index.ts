95ee18f473f4d199e3cd5e35c8d4452cfd79133d
Jaewook Ahn <ajw4586@gmail.com>
2021-08-24 03:18:43 +0900

    Apply redux logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import loggerMiddleware from 'redux-logger';
 import {reducer} from './reducers';
 
 export const store = configureStore({
   reducer,
+  middleware: (middlewares) => middlewares().concat(loggerMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
