43d7e54e7ebc6785b20d628e25eb11adcf072859
GoRyne <korsogogii@kookmin.ac.kr>
2021-08-23 17:50:50 +0900

    Prepare api binding

---
+++ src/redux/apis/activity.ts
@@ -0,0 +1,68 @@
+import {request} from '.';
+
+// 1. Create - 활동 생성
+interface activityCreate {
+  start: string;
+  end: string;
+  place: string;
+  type: 0 | 1 | 2;
+  description: string;
+  participants: string[];
+  private: boolean;
+}
+
+export const createActivity = (data: activityCreate) => {
+  return request.post('/activity/create', {
+    ...data,
+  });
+};
+
+// 2. Search - 활동 검색 (랜딩 페이지)
+export const searchLanding = () => {
+
+};
+
+// 3. Private Search - 활동 검색 (백오피스)
+export const searchPrivate = () => {
+
+};
+
+// 4. Update - 활동 정보 수정
+interface updateRequst {
+  id: string;
+  update: {
+    start: string;
+    end: string;
+    place: string;
+    type: string;
+    description: string;
+    participants: string[];
+  }
+}
+export const updateActivity = (data: updateRequst) => {
+  return request.put('/activity/update', {
+    ...data,
+  });
+};
+
+// 5. Delete - 활동 삭제
+export const deleteActivity = (id: string) => {
+  return request.delete('activity/delete', {
+    data: {id},
+  });
+};
+
+// 6. Upload - 파일 업로드(사진 포함)
+export const uploadFile = () => {
+
+};
+
+// 7. Download - 파일 다운로드
+export const downloadFile = () => {
+
+};
+
+// 8. DeleteFile - 파일 삭제
+export const deleteFile = (id: string, filename: string) => {
+  return request.post('/activity/deletefile', {id, filename});
+};
--- src/redux/apis/fee.ts
+++ src/redux/apis/fee.ts
@@ -1,33 +1,47 @@
 import {request} from '.';
 
+// 주석은 API 명세에 쓰여있는 내용입니다
+// 코드는 명세에 쓰여있는 함수이름을 사용하기 편하게 수정하였습니다.
+
 // 1. Create - 회비내역 초기화 (생성)
-export const create = async (year: number, semester: number, amount: number) => {
+export const createFee = async (year: number, semester: number, amount: number) => {
   return request.post('/fee/create', {year, semester, amount});
 };
 
 // 2. Amount - 회비 납부액 조회
-export const amount = async (id: string, year: number, semester: number) => {
+export const amountListSearch = async (id: string, year: number, semester: number) => {
   return request.post('/fee/amount', {id, year, semester});
 };
 
 // 3. Payers - 회비 납부자 목록 조회
-export const payers = async (year: number, semester: number) => {
+export const payersListSearch = async (year: number, semester: number) => {
   return request.post('/fee/payers', {year, semester});
 };
 
 // 4. Deptors - 회비 미납자 목록 조회
-export const deptors = async (year: number, semester: number) => {
+export const deptorsListSearch = async (year: number, semester: number) => {
   return request.post('fee/deptors', {year, semester});
 };
 
 // 5. Search - 회비 내역 검색 (해당 학기 내역 출력)
-export const search = async (year: number, semester: number) => {
+export const searchTransaction = async (year: number, semester: number) => {
   return request.post('/fee/search', {year, semester});
 };
 
 // 6. Pay - 회비 납부 처리
-export const pay = async (year: number, semester: number, payments: Array<JSON>) => {
-  return request.post('/fee/pay', {year, semester, payments});
+interface PayRequest {
+  year: number;
+  semester: number;
+  payments: {
+    id: string;
+    amount: number;
+  }[];
+}
+
+export const pay = async (data: PayRequest) => {
+  return request.post('/fee/pay', {
+    ...data,
+  });
 };
 
 // 7. deposit - 입금 / 지출 처리 (입금은 양수 지출은 음수)
