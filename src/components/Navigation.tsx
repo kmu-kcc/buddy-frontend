5e75dddd5e16ba67cd232da225180ea49525feee
Jaewook Ahn <ajw4586@gmail.com>
2021-08-25 06:09:56 +0900

    Update navigation hidden rule for SignUpRequests page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -102,7 +102,7 @@ export const Navigation = () => {
     const pathname = location.pathname;
     console.log('pathname updated', pathname);
 
-    if (pathname.match(/\/signin/gi) || pathname.match(/\/signup/gi)) {
+    if (!pathname.startsWith('/organization') && (pathname.match(/\/signin/gi) || pathname.match(/\/signup/gi))) {
       return true;
     }
     return false;
