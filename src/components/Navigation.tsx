a6f41ba75b7ab6489bc181d5ec5ec404d68871d3
Jia <jia5232@naver.com>
2021-08-25 01:22:45 +0900

    Modify Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,8 @@
 import React, {useCallback, useMemo} from 'react';
 import styled from 'styled-components';
 import {useHistory, useLocation} from 'react-router-dom';
-import {Box} from './Box';
-import {Buddy} from './icons/Buddy';
+import {Box, Span, Text} from '../components';
+import {Activity, Organization, Fee, Profile} from '../components/icons';
 
 const Wrapper = styled(Box)`
   width: 240px;
@@ -23,6 +23,7 @@ const IconWrapper = styled(Box)`
 
   :hover {
     background-color: rgba(0, 0, 0, 0.15);
+    border-radius: 20px 20px 20px 20px;
   }
 
   > p {
@@ -34,12 +35,71 @@ const IconWrapper = styled(Box)`
 const Logo = () => {
   const history = useHistory();
   const handleClick = useCallback(() => {
-    history.push('/');
+    history.push('/signin');
   }, [history]);
   return (
-    <IconWrapper isFlex alignItems='center' px='32px' py='20px' fontSize='28px' onClick={handleClick}>
-      <Buddy mr='16px' width='36px' height='50px' color='#fff' />
-      <p>Buddy</p>
+    <IconWrapper isFlex alignItems='center' px='60px' py='20px' onClick={handleClick}>
+      <img src='/images/img_logo.png'></img>
+    </IconWrapper>
+  );
+};
+
+const ActivityTab = () => {
+  const history = useHistory();
+  const handleClick = useCallback(() => {
+    history.push('/activity/list');
+  }, [history]);
+  return (
+    <IconWrapper isFlex alignItems='flexStart' mt='20px' px='60px' py='20px' onClick={handleClick}>
+      <Activity mb='8px' width='20px' height='20px' color='#F9F3FF'></Activity>
+      <Span ml='10px'>
+        <Text fontSize='20px' lineHeight='25px' color='#F9F3FF' >활동 관리</Text>
+      </Span>
+    </IconWrapper>
+  );
+};
+
+const OrganizationTab = () => {
+  const history = useHistory();
+  const handleClick = useCallback(() => {
+    history.push('/organization/members');
+  }, [history]);
+  return (
+    <IconWrapper isFlex alignItems='flexStart' px='60px' py='20px' onClick={handleClick}>
+      <Organization mb='8px' width='20px' height='20px' color='#F9F3FF'></Organization>
+      <Span ml='10px'>
+        <Text fontSize='20px' lineHeight='25px' color='#F9F3FF' >조직 관리</Text>
+      </Span>
+    </IconWrapper>
+  );
+};
+
+const FeeTab = () => {
+  const history = useHistory();
+  const handleClick = useCallback(() => {
+    history.push('/fee/account');
+  }, [history]);
+  return (
+    <IconWrapper isFlex alignItems='flexStart' px='60px' py='20px' onClick={handleClick}>
+      <Fee mb='8px' width='20px' height='20px' color='#F9F3FF'></Fee>
+      <Span ml='10px'>
+        <Text fontSize='20px' lineHeight='25px' color='#F9F3FF' >회계 관리</Text>
+      </Span>
+    </IconWrapper>
+  );
+};
+
+const ProfileTab = () => {
+  const history = useHistory();
+  const handleClick = useCallback(() => {
+    history.push('/user');
+  }, [history]);
+  return (
+    <IconWrapper isFlex alignItems='flexStart' px='60px' py='20px' onClick={handleClick}>
+      <Profile mb='8px' width='20px' height='20px' color='#F9F3FF'></Profile>
+      <Span ml='10px'>
+        <Text fontSize='20px' lineHeight='25px' color='#F9F3FF' >내 정보</Text>
+      </Span>
     </IconWrapper>
   );
 };
@@ -59,6 +119,10 @@ export const Navigation = () => {
   return (
     <Wrapper display={hidden ? 'none' : 'block'}>
       <Logo />
+      <ActivityTab />
+      <OrganizationTab />
+      <FeeTab />
+      <ProfileTab />
     </Wrapper>
   );
 };
