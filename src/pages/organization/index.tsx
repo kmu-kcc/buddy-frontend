cceb12acf552a71586c1493c1651667792ebbb64
Jaewook Ahn <ajw4586@gmail.com>
2021-08-25 08:29:09 +0900

    Improve search input show condition in Organization page

--- src/pages/organization/index.tsx
+++ src/pages/organization/index.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback, useState} from 'react';
-import {Route, useHistory} from 'react-router-dom';
+import React, {useCallback, useMemo, useState} from 'react';
+import {Route, useHistory, useLocation} from 'react-router-dom';
 import {Box, Text, Input, Tab} from '../../components';
 import {Search} from '../../components/icons';
 import {Members} from './Members';
@@ -15,28 +15,28 @@ interface Props {
 
 const Container = ({children}: Props) => {
   const history = useHistory();
+  const location = useLocation();
   const [search, setSearch] = useState('');
+  const searchInputShow = useMemo(() => location.pathname === '/organization/members', [location]);
   const handleSearchChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   }, []);
   const handleTabChange = useCallback((index: number) => {
-    if (history.location.pathname !== paths[index]) {
+    if (location.pathname !== paths[index]) {
       history.push(paths[index]);
     }
-  }, [history]);
+  }, [history, location]);
 
   return (
     <Box isFlex flexDirection='column' width='100%' px='60px'>
       <Box mt='60px' mb='58px' isFlex>
         <Text color='#454440;' fontSize='40px' fontWeight={700}>조직관리</Text>
       </Box>
-      <Box isFlex flexDirection='row-reverse' alignItems='end'>
-        <Input ml='auto' height='59px' width='433px' value={search}
+      <Box isFlex alignItems='start'>
+        <Tab tabs={['동아리원 목록', '입부 신청내역', '퇴부 신청내역']} onTabChange={handleTabChange} />
+        {searchInputShow && <Input ml='auto' height='59px' width='433px' value={search}
           logo={<Search mr='27px' width='24px' height='24px' color='#CBC8BE' />}
-          onChange={handleSearchChange} placeholder='Search' />
-        <Box isFlex flexDirection='column' >
-          <Tab tabs={['동아리원 목록', '입부 신청내역', '퇴부 신청내역']} onTabChange={handleTabChange} />
-        </Box>
+          onChange={handleSearchChange} placeholder='Search' />}
       </Box>
       {children}
     </Box>
