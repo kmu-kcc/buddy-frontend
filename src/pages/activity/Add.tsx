3112ed54f85fff9dc9fecd4815727043743065d0
GoRyne <korsogogii@kookmin.ac.kr>
2021-08-25 01:33:59 +0900

    Layout at activity/add without map

--- src/pages/activity/Add.tsx
+++ src/pages/activity/Add.tsx
@@ -43,15 +43,15 @@ export const Add = () => {
       </Box>
       <Box isFlex mt='56px' alignItems='center'>
         <Text fontSize='20px' color='#454440' lineHeight='24px'>활동 제목</Text>
-        <Input onChange={handleInputChange} value={InputTextValue} ml='29px' width='1036px' />
+        <Input onChange={handleInputChange} value={InputTextValue} ml='49px' width='1036px' height='64px' />
       </Box>
       <Box isFlex mt='30px' alignItems='center'>
         <Text fontSize='20px' color='#454440' lineHeight='24px'>장소</Text>
-        <Input onChange={handlePlaceChange} value={InputPlaceValue} ml='29px' width='1036px' />
+        <Input onChange={handlePlaceChange} value={InputPlaceValue} ml='91px' width='1036px' height='64px' />
       </Box>
       <Box isFlex mt='30px' alignItems='center'>
         <Text fontSize='20px' color='#454440' lineHeight='24px'>할동 종류</Text>
-        <Select ml='29px' width='200px'>
+        <Select ml='49px' width='200px' height='64px'>
           <option>창립제</option>
           <option>스터디</option>
           <option>기타</option>
@@ -59,16 +59,17 @@ export const Add = () => {
       </Box>
       <Box isFlex mt='30px' alignItems='center'>
         <Text fontSize='20px' color='#454440' lineHeight='24px'>기간</Text>
-        <Input onChange={handleStartChange} value={InputStartValue} width='208px' ml='29px' mr='20px'></Input>
+        <Input onChange={handleStartChange} value={InputStartValue} width='208px' height='64px' ml='91px' mr='20px'></Input>
         <p> ~ </p>
-        <Input onChange={handleEndChange} value={InputEndValue} width='208px' ml='20px'></Input>
+        <Input onChange={handleEndChange} value={InputEndValue} width='208px' height='64px'></Input>
       </Box>
-      <Box mt='30px' alignItems='center'>
+      <Box isFlex mt='30px' alignItems='center'>
         <Text fontSize='20px' color='#454440' lineHeight='24px'>참여자</Text>
+        <Button ml='72px'>이름 검색 및 추가</Button>
       </Box>
       <Box isFlex flexDirection='column' mt='60px'>
         <Text fontSize='20px' color='#454440' lineHeight='24px'>본문</Text>
-        <Textarea onChange={handleDescriptionareaChange} value={descriptionValue} width='1148px' height='838px' mt='20px'></Textarea>
+        <Textarea onChange={handleDescriptionareaChange} value={descriptionValue} width='1168px' height='838px' mt='20px'></Textarea>
       </Box>
       <FloatButton>저장하기</FloatButton>
     </Box>
