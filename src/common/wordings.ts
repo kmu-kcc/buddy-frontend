47e41eb3d494f3ecb7731b8fc490d17dd78c2816
Jaewook Ahn <ajw4586@gmail.com>
2021-08-25 06:22:54 +0900

    Add wordings for failure case

--- src/common/wordings.ts
+++ src/common/wordings.ts
@@ -5,17 +5,22 @@ export const CommonMessage = {
 
 export const SignInMessage = {
   empty: '아이디 / 비밀번호를 입력해주세요.',
+  fail: '로그인에 실패했습니다.',
 };
 
 export const SignUpMessage = {
   empty: '모든 필드를 입력해주세요.',
+  fail: '회원가입에 실패했습니다.',
 };
 
 export const SettingsMessage = {
   empty: '모든 필드를 입력해주세요.',
   updateSuccess: '정보가 저장되었습니다.',
+  updateFail: '정보를 저장하는 중 문제가 발생했습니다.',
+  loadingFail: '정보를 불러오는 중 문제가 발생했습니다.',
 };
 
 export const MemberRequestsMessage = {
   loadingSuccess: '데이터를 불러왔습니다.',
+  loadingFail: '데이터를 불러오는 중 문제가 발생했습니다.',
 };
