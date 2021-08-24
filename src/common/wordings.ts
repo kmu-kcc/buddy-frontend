2a0e1010c67b7424caa12f555171cd8e23909964
Jaewook Ahn <ajw4586@gmail.com>
2021-08-25 02:20:09 +0900

    Add wordings file to manage wording texts

---
+++ src/common/wordings.ts
@@ -0,0 +1,12 @@
+export const CommonMessage = {
+  loading: '처리중입니다. 잠시만 기다려주세요.',
+  error: '에러가 발생했습니다. 잠시 후에 다시 시도해주세요.',
+};
+
+export const SignInMessage = {
+  empty: '아이디 / 비밀번호를 입력해주세요.',
+};
+
+export const SignUpMessage = {
+  empty: '모든 필드를 입력해주세요.',
+};
