4dbe45a1e3b3765c50e81a2202e5be97bb32b898
Jaewook Ahn <ajw4586@gmail.com>
2021-08-25 15:51:35 +0900

    Remove unused code

--- src/pages/organization/WIthdrawRequests.tsx
+++ src/pages/organization/WIthdrawRequests.tsx
@@ -7,11 +7,6 @@ import {Box, Button, MemberCard, Popup, Text, Span} from '../../components';
 import {CommonMessage, MemberRequestsMessage} from '../../common/wordings';
 import {User} from '../../models/User';
 
-// const ReverseButton = styled(Button)`
-//   background: #FF6845;
-//   border: 2px solid #FF6845;
-// `;
-
 export const WithdrawRequests = () => {
   const dispatch = useDispatch();
   const {withdrawalRequests, loadingDeleteMemberRequests} = useSelector((state: RootState) => state.member);
@@ -72,16 +67,6 @@ export const WithdrawRequests = () => {
   return (
     <Box>
       <Box isFlex flexDirection='row-reverse' mt='36px'>
-        {/* <Box>
-          <ReverseButton mr='114px' ml='21px' width='149px' height='54px' onClick={handleWithdrawalRequestPopupClick}>거부</ReverseButton>
-          <Popup type='danger'confirmLabel='거절' cancelLabel='닫기'
-            onClose={handleWithdrawalClose}
-            onConfirm={handleWithdrawalConfirm}
-            onCancel={handleWithdrawalCancel}
-            show={withdrawalPopupShow}>
-            <Text fontSize='20px' lineHeight='25px'><Span fontWeight={700}>퇴부</Span>를 거절하시겠습니까?</Text>
-          </Popup>
-        </Box> */}
         <Box>
           <Button width='149px' height='54px' onClick={handleWithdrawalRequestPopupClick}>승인</Button>
           <Popup type='primary' confirmLabel='승인' cancelLabel='닫기'
