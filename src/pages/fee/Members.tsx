ff5efaadbbd60e2bf34a52c71a18f570195ce887
Jaewook Ahn <ajw4586@gmail.com>
2021-08-26 02:50:19 +0900

    Improve Fee Members page
    - bind member paid progress bar
    - bind searchDeptors API
    - adjust layout

--- src/pages/fee/Members.tsx
+++ src/pages/fee/Members.tsx
@@ -3,18 +3,11 @@ import styled from 'styled-components';
 import {toast} from 'react-toastify';
 import {useSelector} from 'react-redux';
 import {RootState, useDispatch} from '../../store';
-import {searchPayers} from '../../store/actions/feeActions';
+import {searchPayers, searchDeptors} from '../../store/actions/feeActions';
 import {Box, Input, Text, Span, Tab} from '../../components';
 import {Filter, Search} from '../../components/icons';
 import {CommonMessage, FeeMessage} from '../../common/wordings';
 import {getCurrentSemester} from '../../utils/semester';
-
-const GroupName = styled.span`
-  font-size: 24px;
-  line-height: 29px;
-  font-weight: normal;
-  color: #000000;
-`;
 
 const NameText = styled(Text)`
   font-weight: bold;
@@ -29,31 +22,6 @@ const UnivNumText = styled(Text)`
   line-height: 17px;
   color: #8D8D8D;
 `;
-
-const FilterText = styled(Text)`
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 25px;
-  color: #8D8C85;
-`;
-
-const UnpaidMembersProfile = [
-  {
-    id: 1,
-    username: '홍길동',
-    univnumber: '20190155',
-  },
-  {
-    id: 2,
-    username: '홍길동',
-    univnumber: '20190155',
-  },
-  {
-    id: 3,
-    username: '홍길동',
-    univnumber: '20190155',
-  },
-];
 
 interface MemberCardProps {
   username?: string;
@@ -72,12 +40,16 @@ const MemberCard = (MemberCardProps: MemberCardProps) => {
 
 export const Members = () => {
   const dispatch = useDispatch();
-  const {loadingPayers, payers} = useSelector((state: RootState) => state.fee);
+  const {loadingPayers, loadingDeptors, payers, deptors} = useSelector((state: RootState) => state.fee);
   const [InputTextValue, setInputTextValue] = useState('');
   const handleInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setInputTextValue(event.target.value);
   }, [setInputTextValue]);
   const empty = useMemo(() => InputTextValue === '', [InputTextValue]);
+  const totalUserCount = useMemo(() => payers.length + deptors.length, [payers.length, deptors.length]);
+  const paidPercent = useMemo(() => {
+    return payers.length / totalUserCount * 100;
+  }, [payers.length, totalUserCount]);
 
   const fetchPayers = useCallback(async () => {
     if (loadingPayers) {
@@ -100,9 +72,32 @@ export const Members = () => {
       toast.error(CommonMessage.error);
     }
   }, [dispatch, loadingPayers]);
+  const fetchDeptors = useCallback(async () => {
+    if (loadingDeptors) {
+      toast.info(CommonMessage.loading);
+      return;
+    }
+
+    try {
+      const response = await dispatch(searchDeptors({
+        ...getCurrentSemester(),
+      }));
+
+      if (response.type === searchDeptors.fulfilled.type) {
+        toast.success(FeeMessage.successDeptors);
+      } else {
+        toast.error(response.payload);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error(CommonMessage.error);
+    }
+  }, [dispatch, loadingDeptors]);
 
   useEffect(() => {
     fetchPayers();
+    fetchDeptors();
+  //  eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // const PaidMembers = PaidMembersProfile.map((info, idx) => (
@@ -110,11 +105,11 @@ export const Members = () => {
   //   </Box>
   // ));
 
-  const UnpaidMembers = UnpaidMembersProfile.map((info, idx) => (
-    <Box border='1px solid #FF6845' borderRadius='15px' key={idx}>
-      <MemberCard username={info.username} univnumber={info.univnumber} />
-    </Box>
-  ));
+  // const UnpaidMembers = UnpaidMembersProfile.map((info, idx) => (
+  //   <Box border='1px solid #FF6845' borderRadius='15px' key={idx}>
+  //     <MemberCard username={info.username} univnumber={info.univnumber} />
+  //   </Box>
+  // ));
 
   return (
     <Box width='100%' py='48px' px='60px'>
@@ -124,30 +119,36 @@ export const Members = () => {
         <Input empty={empty} logo={<Search mr='27px' width='24px' height='24px' color='#CBC8BE' />} onChange={handleInputChange} value={InputTextValue} placeholder='search' />
       </Box>
       <Box ml='7px' isFlex width='100%' height='30px' mt='64px' alignItems='baseline'>
-        <Span color='#454440' fontSize='28px' lineHeight='34px' fontWeight={700}>3,200,000 원 /</Span>
-        <Span ml='10px' color='rgba(69, 68, 64, 0.5)' fontSize='24px' lineHeight='29px'>4,000,000원</Span>
+        <Span color='#454440' fontSize='28px' lineHeight='34px' fontWeight={700}>{payers.length}명 /</Span>
+        <Span ml='10px' color='rgba(69, 68, 64, 0.5)' fontSize='24px' lineHeight='29px'>{totalUserCount}명</Span>
       </Box>
       <Box ml='-10px' mt='15px' isFlex width='100%' height='34px' borderRadius='73px' bg='#EFEBFC'>
-        <Box isFlex width='940px' height='34px' bg='#6D48E5' borderRadius='73px' />
+        <Box isFlex width={`${paidPercent}%`} height='34px' bg='#6D48E5' borderRadius='73px' />
       </Box>
       <Box mt='62px' isFlex width='100%' flexDirection='column'>
         <Box isFlex alignItems='center'>
           <Text color='#000' fontSize='24px' lineHeight='29px'>납부자</Text>
-          <Box isFlex ml='auto'>
-            <Filter mb='8px' height='24px' color='#8D8C85' />
-            <FilterText>필터</FilterText>
+          <Box isFlex ml='auto' cursor='pointer'>
+            <Filter height='24px' color='#8D8C85' />
+            <Span ml='7px' color='#8D8C85' fontSize='20px' lineHeight='25px' fontWeight={500}>필터</Span>
           </Box>
         </Box>
         <Box mt='30px' isFlex flexWrap='wrap' style={{gap: '20px'}}>
-          {payers.length > 0 ? payers.map((payer) => <MemberCard key={payer.id} username={payer.name} univnumber={payer.id} />) : <Text width='100%' textAlign='center'>납부한 회원이 없습니다.</Text>}
+          {payers.length > 0 ? payers.map((payer) => (
+            <MemberCard key={payer.id}
+              username={payer.name}
+              univnumber={payer.id} />
+          )) : <Text mt='32px' width='100%' textAlign='center'>납부한 회원이 없습니다.</Text>}
         </Box>
       </Box>
-      <Box isFlex width='100%' mt='64px' mb='28px' flexDirection='column'>
-        <Box isBlock width='140px' height='30px' mt='64px' position='relative' mb='28px'>
-          <GroupName>미납부자</GroupName>
-        </Box>
-        <Box isFlex flexWrap='wrap' style={{gap: '30px'}}>
-          {UnpaidMembers}
+      <Box mt='62px' isFlex width='100%' mb='28px' flexDirection='column'>
+        <Text color='#000' fontSize='24px' lineHeight='29px'>미납부자</Text>
+        <Box mt='30px' isFlex flexWrap='wrap' style={{gap: '20px'}}>
+          {deptors.length > 0 ? deptors.map((deptor) => (
+            <MemberCard key={deptor.id}
+              username={deptor.name}
+              univnumber={deptor.id} />
+          )) : <Text mt='32px' width='100%' textAlign='center'>미납부한 회원이 없습니다.</Text>}
         </Box>
       </Box>
     </Box>
