9716052cc53bc3c635f18f2919110eea0ea32155
Jaewook Ahn <ajw4586@gmail.com>
2021-08-26 01:20:22 +0900

    Add semester type in getCurrentSemester util method

--- src/utils/semester.ts
+++ src/utils/semester.ts
@@ -1,4 +1,6 @@
-export const getCurrentSemester = () => {
+import {Semester} from '../models/Fee';
+
+export const getCurrentSemester = (): Semester => {
   const date = new Date();
   return {
     semester: date.getMonth() < 8 ? 1 : 2,
