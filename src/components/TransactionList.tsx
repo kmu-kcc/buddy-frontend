2ac7769ab6850ea82e95e0835c59e9d3ae80d5ee
Jaewook Ahn <ajw4586@gmail.com>
2021-08-25 23:32:49 +0900

    Refactor TransactionList component

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,91 +1,81 @@
 import React from 'react';
 import styled from 'styled-components';
+import {format} from 'date-fns';
 import {Box} from './Box';
 import {Text} from './Text';
-
-const ListLine = styled.div`
-  width: 821px;
-  height: 0px;
-  border: 1px solid #CBC8BE;
-`;
+import {formatCurrency} from '../utils/currency';
 
 const Dummy = [
   {
     id: 1,
-    date: '21.08.15',
-    describtion: '회비납부',
+    date: '1629901188990',
+    description: '회비납부',
     amount: 12000,
     total: 76000,
     type: 'input',
   },
   {
     id: 2,
-    date: '21.08.15',
-    describtion: '회비납부',
+    date: '1629901188990',
+    description: '회비납부',
     amount: 12000,
     total: 88000,
     type: 'input',
   },
   {
     id: 3,
-    date: '21.08.15',
-    describtion: '회비납부',
+    date: '1629901188990',
+    description: '회비납부',
     amount: 12000,
     total: 100000,
     type: 'input',
   },
   {
     id: 4,
-    date: '21.08.15',
-    describtion: '회비납부',
+    date: '1629901188990',
+    description: '회비납부',
     amount: -12000,
     total: 88000,
     type: 'withdraw',
   },
 ];
 
-const ColorText = styled(Text)<{amount: number}>`
-  font-size : 20px;
+const ColoredText = styled(Text)<{amount: number;}>`
+  font-size: 20px;
   line-height: 25px;
+  font-weight: 700;
   color: ${({amount}) => amount < 0 ? '#FF6845' : '#6D48E5'};
 `;
 
-const comma = (num: number) =>{
-  const res = num.toLocaleString();
-  return (num >= 0 ? '+'+res : res);
-};
-
-interface TransActionInterface {
-  date?: string;
-  describtion?: string;
+interface TransactionProps {
+  date: string;
+  description: string;
   amount: number;
   total: number;
 };
 
-export const AccountList = (props: TransActionInterface) => {
-  const {date, describtion, amount, total} = props;
+export const Transaction = (props: TransactionProps) => {
+  const {date, description, amount, total} = props;
   return (
-    <div>
-      <Box isFlex mb='16px' mt='27px'>
-        <Text color='#8D8C85' fontSize='20px' lineHeight='25px' ml='32px' height='35px' width='114px'>{date}</Text>
-        <Text color='#000000' fontSize='20px' lineHeight='25px' ml='54px' height='35px' width='198px'>{describtion}</Text>
-        <ColorText amount={amount} ml='97px' height='35px' width='100px'> {comma(amount)}</ColorText>
-        <Text color='#8D8C85' fontSize='20px' lineHeight='25px' ml='42px'>{comma(total)}</Text>
-      </Box>
-      <ListLine />
-    </div>
+    <Box isFlex py='27px' borderBottom='1px solid #CBC8BE'>
+      <Text ml='32px' width='114px' color='#8D8C85' fontSize='20px' lineHeight='25px' fontWeight={500}>{format(Number(date), 'yy.MM.dd')}</Text>
+      <Text ml='54px' color='#000' fontSize='20px' lineHeight='25px' fontWeight={500} width='198px'>{description}</Text>
+      <ColoredText ml='97px' width='100px' amount={amount}>{formatCurrency(amount)}</ColoredText>
+      <Text ml='42px' color='#8D8C85' fontSize='20px' lineHeight='25px' fontWeight={500}>{formatCurrency(total)}</Text>
+    </Box>
   );
 };
 
 export const TransactionList = () => {
-  const List = Dummy.map((info, idx) => (
-    <AccountList key={idx} date={info.date} describtion={info.describtion} amount={info.amount} total={(info.total)} />
-  ));
   return (
-    <div>
-      <Box>
-        {List}
-      </Box>
-    </div>
+    <Box>
+      {Dummy.map((info, idx) => (
+        <Transaction key={idx}
+          date={info.date}
+          description={info.description}
+          amount={info.amount}
+          total={(info.total)} />
+      ))}
+    </Box>
   );
 };
