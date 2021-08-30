6382b34d89a7218b7008563d7274d4fc7805066c
Jaewook Ahn <ajw4586@gmail.com>
2021-08-31 05:56:27 +0900

    Bind Fee pay and exempt API
    -- implement popups
    - add wordings for APIs

--- src/pages/fee/Members.tsx
+++ src/pages/fee/Members.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import {toast} from 'react-toastify';
 import {useSelector} from 'react-redux';
 import {RootState, useDispatch} from '../../store';
-import {searchPayers, searchDeptors, createFee} from '../../store/actions/feeActions';
+import {searchPayers, searchDeptors, createFee, pay, exempt} from '../../store/actions/feeActions';
 import {Box, Button, Input, Text, Popup, Span} from '../../components';
 import {Filter} from '../../components/icons';
 import {CommonMessage, FeeMessage} from '../../common/wordings';
@@ -23,25 +23,32 @@ const UnivNumText = styled(Text)`
 `;
 
 interface MemberCardProps {
-  username?: string;
-  univnumber?: string;
+  name: string;
+  id: string;
+  border: string;
+  onClick?: () => void;
 }
 
 const MemberCard = (MemberCardProps: MemberCardProps) => {
-  const {username, univnumber} = MemberCardProps;
+  const {name, id, border, onClick} = MemberCardProps;
   return (
-    <Box maxWidth='248px' isFlex pb='20px' pt='20px' px='44px' alignItems='center'>
-      <NameText fontWeight={500} fontSize='16px' lineHeight='20px'>{username}</NameText>
-      <UnivNumText ml='40px' fontWeight={500} fontSize='16px' lineHeight='20px'>{univnumber}</UnivNumText>
+    <Box maxWidth='248px' isFlex pb='20px' pt='20px' px='44px' alignItems='center' borderRadius='15px' border={border} onClick={onClick}>
+      <NameText fontWeight={500} fontSize='16px' lineHeight='20px'>{name}</NameText>
+      <UnivNumText ml='40px' fontWeight={500} fontSize='16px' lineHeight='20px'>{id}</UnivNumText>
     </Box>
   );
 };
 
 export const Members = () => {
   const dispatch = useDispatch();
-  const {currentSemester, loadingCreateFee, loadingPayers, loadingDeptors, payers, deptors} = useSelector((state: RootState) => state.fee);
+  const {currentSemester, loadingCreateFee, loadingPayers, loadingDeptors, loadingPay, loadingExempt, payers, deptors} = useSelector((state: RootState) => state.fee);
   const [startFeePopupShow, setStartFeePopupShow] = useState(false);
+  const [payPopupShow, setPayPopupShow] = useState(false);
+  const [payerId, setPayerId] = useState('');
+  const [payerName, setPayerName] = useState('');
+  const [payAmount, setPayAmount] = useState('0');
   const [amount, setAmount] = useState('0');
+
   const totalUserCount = useMemo(() => payers.length + deptors.length, [payers.length, deptors.length]);
   const paidPercent = useMemo(() => {
     return payers.length / totalUserCount * 100;
@@ -93,6 +100,9 @@ export const Members = () => {
   const handleAmountChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setAmount(event.target.value);
   }, []);
+  const handlePayAmountChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setPayAmount(event.target.value);
+  }, []);
   const handleStartFeePopupClick = useCallback(() => setStartFeePopupShow(true), []);
   const handleStartFeePopupClose = useCallback(() => setStartFeePopupShow(false), []);
   const handleStartFeePopupConfirm = useCallback(async () => {
@@ -102,7 +112,7 @@ export const Members = () => {
     }
 
     if (Number(amount) === NaN) {
-      toast.error(FeeMessage.invalidFeeAmount);
+      toast.warn(FeeMessage.invalidFeeAmount);
       return;
     }
 
@@ -117,23 +127,80 @@ export const Members = () => {
       toast.error(response.payload as unknown as string);
     }
   }, [dispatch, loadingCreateFee, amount, currentSemester]);
+  const handlePayPopupClick = useCallback((id: string, name: string) => () => {
+    setPayerId(id);
+    setPayerName(name);
+    setPayPopupShow(true);
+  }, []);
+  const handlePayPopupClose = useCallback(() => setPayPopupShow(false), []);
+  const handlePayPopupConfirm = useCallback(async () => {
+    if (loadingPay) {
+      toast.info(CommonMessage.loading);
+      return;
+    }
+
+    if (!payerId || Number(payAmount) === NaN) {
+      toast.warn(FeeMessage.invalidPayInfo);
+      return;
+    }
+
+    try {
+      const response = await dispatch(pay({
+        ...currentSemester,
+        payments: [{
+          id: payerId,
+          amount: Number(payAmount),
+        }],
+      }));
+
+      if (response.type === pay.fulfilled.type) {
+        toast.success(FeeMessage.successPay);
+        fetchDeptors();
+        fetchPayers();
+      } else {
+        toast.error(response.payload as unknown as string);
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error(CommonMessage.error);
+    }
+  }, [dispatch, loadingPay, payerId, payAmount, currentSemester, fetchDeptors, fetchPayers]);
+  const handleExemptClick = useCallback(async () => {
+    if (loadingExempt) {
+      toast.info(CommonMessage.loading);
+      return;
+    }
+
+    if (!payerId) {
+      toast.warn(FeeMessage.invalidPayInfo);
+      return;
+    }
+
+    try {
+      const response = await dispatch(exempt({
+        id: payerId,
+        ...currentSemester,
+      }));
+
+      if (response.type === exempt.fulfilled.type) {
+        toast.success(FeeMessage.successExempt);
+        setPayPopupShow(false);
+        fetchDeptors();
+        fetchPayers();
+      } else {
+        toast.error(response.payload as unknown as string);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error(CommonMessage.error);
+    }
+  }, [dispatch, loadingExempt, payerId, currentSemester, fetchPayers, fetchDeptors]);
 
   useEffect(() => {
     fetchPayers();
     fetchDeptors();
   //  eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  // const PaidMembers = PaidMembersProfile.map((info, idx) => (
-  //   <Box border='1px solid #6D48E5' borderRadius='15px' key={idx}>
-  //   </Box>
-  // ));
-
-  // const UnpaidMembers = UnpaidMembersProfile.map((info, idx) => (
-  //   <Box border='1px solid #FF6845' borderRadius='15px' key={idx}>
-  //     <MemberCard username={info.username} univnumber={info.univnumber} />
-  //   </Box>
-  // ));
 
   return (
     <Box isFlex width='100%' flexDirection='column'>
@@ -145,7 +212,7 @@ export const Members = () => {
         <Button height='52px' py='0px' lineHeight='52px' ml='auto' mr='10px' onClick={handleStartFeePopupClick}>학기 시작</Button>
       </Box>
       <Box ml='-10px' mt='15px' isFlex width='100%' height='34px' borderRadius='17px' bg='#EFEBFC'>
-        <Box isFlex width={`${paidPercent}%`} height='34px' bg='#6D48E5' borderRadius='73px' />
+        <Box isFlex width={`${paidPercent}%`} height='34px' bg='#6D48E5' borderRadius='73px' style={{transition: 'all 0.2s ease-out'}} />
       </Box>
       <Box mt='62px' isFlex width='100%' flexDirection='column'>
         <Box isFlex alignItems='center'>
@@ -158,8 +225,9 @@ export const Members = () => {
         <Box mt='30px' isFlex flexWrap='wrap' style={{gap: '20px'}}>
           {payers.length > 0 ? payers.map((payer) => (
             <MemberCard key={payer.id}
-              username={payer.name}
-              univnumber={payer.id} />
+              border='1px solid #6D48E5'
+              name={payer.name}
+              id={payer.id} />
           )) : <Text mt='32px' width='100%' textAlign='center'>납부한 회원이 없습니다.</Text>}
         </Box>
       </Box>
@@ -168,21 +236,36 @@ export const Members = () => {
         <Box mt='30px' isFlex flexWrap='wrap' style={{gap: '20px'}}>
           {deptors.length > 0 ? deptors.map((deptor) => (
             <MemberCard key={deptor.id}
-              username={deptor.name}
-              univnumber={deptor.id} />
+              border='1px solid #D5D5D5'
+              name={deptor.name}
+              id={deptor.id}
+              onClick={handlePayPopupClick(deptor.id, deptor.name)} />
           )) : <Text mt='32px' width='100%' textAlign='center'>미납부한 회원이 없습니다.</Text>}
         </Box>
       </Box>
-      <Popup type='primary' height='fit-content' show={startFeePopupShow}
+      <Popup type='primary' width='fit-content' height='fit-content' show={startFeePopupShow}
+        p='30px' marginContentBottom='30px' marginButton='20px'
         confirmLabel='시작' cancelLabel='닫기'
         onConfirm={handleStartFeePopupConfirm}
         onClose={handleStartFeePopupClose}>
-        <Box isFlex flexDirection='column' alignItems='center'>
-          <Text fontSize='20px' lineHeight='25px'>{currentSemester.year}년도 {currentSemester.semester}학기 회계를 시작할까요?</Text>
-          <Box isFlex mt='24px' alignItems='center' justifyContent='center'>
-            <Text fontSize='20px' lineHeight='25px' mr='24px'>회비 금액</Text>
-            <Input maxWidth='45%' flex={1} type='number' value={amount} placeholder='금액 입력' onChange={handleAmountChange} />
+        <Box isFlex width='100%' flexDirection='column'>
+          <Text color='#454440' fontSize='20px' lineHeight='25px' fontWeight={700}>{currentSemester.year}년도 {currentSemester.semester}학기 회계를 시작할까요?</Text>
+          <Text mt='28px' color='#454440' fontSize='16px' lineHeight='19px' mr='24px'>회비 금액</Text>
+          <Input mt='12px' width='100%' type='number' value={amount} placeholder='금액 입력' onChange={handleAmountChange} />
+        </Box>
+      </Popup>
+      <Popup type='primary' width='fit-content' height='fit-content' show={payPopupShow}
+        p='30px' marginContentBottom='30px' marginButton='20px'
+        confirmLabel='저장' cancelLabel='취소'
+        onConfirm={handlePayPopupConfirm}
+        onClose={handlePayPopupClose}>
+        <Box isFlex width='100%' flexDirection='column'>
+          <Box isFlex width='100%' alignItems='center' justifyContent='space-between'>
+            <Text color='#454440' fontSize='18px' lineHeight='22px' fontWeight={700}>{payerName}의 회비 내역</Text>
+            <Button height='24px' py='0' px='17px' fontSize='12px' lineHeight='14px' border='none' background='#F5B81C' onClick={handleExemptClick}>면제</Button>
           </Box>
+          <Text mt='28px' color='#454440' fontSize='16px' lineHeight='19px'>납부한 금액</Text>
+          <Input mt='12px' width='100%' type='number' value={payAmount} onChange={handlePayAmountChange} />
         </Box>
       </Popup>
     </Box>
