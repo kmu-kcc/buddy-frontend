7e38c8fb69b34e03716dc0987c729edf25a1d492
Jaewook Ahn <ajw4586@gmail.com>
2021-08-30 22:00:00 +0900

    Add role check logic on Activity router
    - show no permission message if user does not have role

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
+import React, {useLayoutEffect} from 'react';
+import {Route, useHistory} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {toast} from 'react-toastify';
+import {RootState} from '../../store';
 import {Router} from '../../common/router';
 import {List} from './List';
 import {Create} from './Create';
 import {Detail} from './Detail';
 import {Edit} from './Edit';
+import {CommonMessage} from '../../common/wordings';
 
-export const Activity = () => (
-  <Router authentication>
-    <Route path='/activity/create' exact component={Create} />
-    <Route path='/activity/detail' exact component={Detail} />
-    <Route path='/activity' exact component={List} />
-    <Route path='/activity/edit' exact component={Edit} />
-  </Router>
-);
+export const Activity = () => {
+  const history = useHistory();
+  const {user} = useSelector((state: RootState) => state.user);
+
+  useLayoutEffect(() => {
+    if (!user?.role?.activity_management) {
+      toast.error(CommonMessage.noPermission);
+      history.replace('/user');
+    }
+  }, [history, user]);
+
+  return (
+    <Router authentication>
+      <Route path='/activity' exact component={List} />
+      <Route path='/activity/create' exact component={Create} />
+      <Route path='/activity/detail' exact component={Detail} />
+      <Route path='/activity/edit' exact component={Edit} />
+    </Router>
+  );
+};
