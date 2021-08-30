320858476ecb343e8bb1f367a9280ab5424602bc
Jaewook Ahn <ajw4586@gmail.com>
2021-08-30 22:09:50 +0900

    Apply role check logic on Fee router

--- src/pages/fee/index.tsx
+++ src/pages/fee/index.tsx
@@ -1,20 +1,31 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {Route, useHistory, useLocation} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../store';
 import {Box, Select, Tab, Button, Input, Text} from '../../components';
 import {Router} from '../../common/router';
 import {Account} from './Account';
 import {Members} from './Members';
 
 const paths = ['/fee/account', '/fee/members'];
+const menus = ['입출금내역 목록', '동아리원 목록'];
 
 interface Props {
   children: JSX.Element;
 }
 
 const Container = ({children}: Props) => {
+  const {user} = useSelector((state: RootState) => state.user);
   const history = useHistory();
   const location = useLocation();
   const [InputTextValue, setInputTextValue] = useState('');
+  const tabs = useMemo(() => {
+    if (user?.role?.fee_management) {
+      return menus;
+    } else {
+      return [menus[0]];
+    }
+  }, [user]);
 
   const handleInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setInputTextValue(event.target.value);
@@ -29,7 +40,7 @@ const Container = ({children}: Props) => {
     <Box isFlex flexDirection='column' width='100%' px='60px'>
       <Text mt='60px' mb='58px' color='#454440' fontSize='40px' fontWeight={700} lineHeight='50px'>회계관리</Text>
       <Box isFlex alignItems='start'>
-        <Tab tabs={['입출금내역 목록', '동아리원 목록']} onTabChange={handleTabChange} />
+        <Tab tabs={tabs} onTabChange={handleTabChange} />
         <Box ml='auto' isFlex>
           <Input width='150px' onChange={handleInputChange} value={InputTextValue} placeholder='연도' />
           <Select width='150px' ml='20px' placeholder='학기'>
