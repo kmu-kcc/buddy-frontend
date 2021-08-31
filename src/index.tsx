96dd47d3f307594fcd66f5353ce7d1318062dca4
Jaewook Ahn <ajw4586@gmail.com>
2021-08-31 19:02:06 +0900

    Add sentry support for tracking errors in production

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import {Integrations} from '@sentry/tracing';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
+
+if (process.env.REACT_APP_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://6bd1c8df0ace4d2ba6a8e589df6ac78a@o983297.ingest.sentry.io/5938930',
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
     <React.StrictMode>
