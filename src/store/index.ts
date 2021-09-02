ea7ac1c76f40ad3165b08e0a7af72fbc5de77e3a
Jaewook Ahn <ajw4586@gmail.com>
2021-09-03 05:00:03 +0900

    Fix redux logger middleware to do not include in production

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, Middleware} from '@reduxjs/toolkit';
 import {useDispatch as useReduxDispatch} from 'react-redux';
 import loggerMiddleware from 'redux-logger';
 import {reducer} from './reducers';
+import {isProduction} from '../utils/env';
+
+const middleware: Middleware[] = isProduction() ? [] : [loggerMiddleware];
 
 export const store = configureStore({
   reducer,
-  middleware: (middlewares) => middlewares().concat(loggerMiddleware),
+  middleware: (middlewares) => middlewares().concat(middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
