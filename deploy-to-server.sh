83ccc8f79ca30b5fad6adb6027773fb6c786b9fe
Jaewook Ahn <ajw4586@gmail.com>
2021-10-24 17:10:39 +0900

    Update deploy script

--- deploy-to-server.sh
+++ deploy-to-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -i buddy-instance.key -r ~/projects/kmu/kcc/projects/buddy-frontend/build ubuntu@146.56.190.179:/home/ubuntu/buddy-frontend
+scp -i ../../../../cert/oci/buddy-instance.key -r ~/projects/kmu/kcc/projects/buddy-frontend/build ubuntu@146.56.190.179:/home/ubuntu/buddy-frontend
